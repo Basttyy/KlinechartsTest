3a60d408c45e80dee37ff258975109b11f18642b
Abdulbasit <basttyy@gmail.com>
2025-10-08 14:42:31 +0100

    fix vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss()
-  ],
-  base: process.env.VITE_DOMAIN_PATH as string,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      tailwindcss()
+    ],
+    base: env.VITE_DOMAIN_PATH,
+  }
 })
